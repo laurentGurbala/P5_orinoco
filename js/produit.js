8ff757e0922880ac4889ce6a7c9e9b6caf6d618c
laurentGurbala <laurent.gurbala@gmail.com>
2020-12-27 15:12:07 +0100

    fix du commit précédent
    suppression involontaire.

--- js/produit.js
+++ js/produit.js
@@ -12,11 +12,38 @@ const id = params.get("_id"); // récupére ce qui se situe après [id=] donc l'
 
 // console.log(id);
 
+// Fonction qui permet de remplacer le template par les bonne valeurs de l'api
+function remplaceLeTemplate(nounours) {
+    // Remplace l'image
+    const imageNounours = document.getElementById("image-nounours");
+    imageNounours.src = nounours.imageUrl;
 
+    // Remplace le nom
+    const nomNounours = document.getElementById("nom-nounours");
+    nomNounours.textContent = nounours.name;
+
+    // Remplace la description
+    const descriptionNounours = document.getElementById("description-nounours");
+    descriptionNounours.textContent = nounours.description;
+
+    // Incrémente les options de couleur
+    const couleurNounours = document.getElementById("couleur");
+
+    // Parcours les couleurs disponibles dans l'api et les ajoutes dans les options
+    for(let i = 0; i < nounours.colors.length; i++) {
+        let couleurOption = document.createElement("option");
+        couleurOption.textContent = nounours.colors[i];
+        couleurNounours.append(couleurOption);
+    }
+
+    // Remplace le prix
+    const prixNounours = document.getElementById("prix-nounours");
+    prixNounours.textContent = nounours.price + " €";
+}
 
 // Ajouter le produit au localStorage
 function ajoutProduitDansLocalStorage(produit) {
-
+    
 }
 
 // récupération de l'api d'un nounours
